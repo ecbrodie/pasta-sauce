fbff0f2cb995c9b8450da5c89eda3bbf87d8153f
Evan Brodie <brodie.evan@gmail.com>
2018-05-14 01:09:02 -0400

    installs gorilla, gin and gometalinter

--- main.go
+++ main.go
@@ -6,14 +6,16 @@ import (
 	"net/http"
 
 	"github.com/ecbrodie/pasta-sauce/models"
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	evan := models.NewUser("Evan")
 	fmt.Fprintf(w, "Hello %s\n", evan.Name)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", Handler)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
